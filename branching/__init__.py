fd6fde30797d47bc6e089321b59f90ef95eaf482
John C Barstow <jbowtie@amathaine.com>
2013-01-03 15:44:40 +1300

    Initial version

---
+++ branching/__init__.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+'''branching
+
+Manage geature branches.
+'''
+
+from mercurial import commands, hg
+
+def harvest(ui, repo, branch, **opts):
+    """Merge a branch into default"""
+    if branch not in repo.branchtags():
+        ui.write("Branch %s does not exist! (use 'hg branches' to get a list of branches)\n" % branch)
+        return
+
+    #todo: verify branch is open and has one head
+    hg.clean(repo, branch)
+    repo.commit("Closed branch %s" % branch, opts.get('user'), opts.get('date'), None, extra={'close':1})
+    hg.clean(repo, "default")
+    hg.merge(repo, branch)
+    repo.commit("Merged %s" % branch, opts.get('user'), opts.get('date'), None)
+    ui.write("Completed merge of %s\n" % branch)
+
+def close_branch(ui, repo, branch, **opts):
+    """Close a branch"""
+    if branch not in repo.branchtags():
+        ui.write("Branch %s does not exist! (use 'hg branches' to get a list of branches)\n" % branch)
+        return
+
+    #todo: verify branch is open and has one head
+    hg.clean(repo, branch)
+    repo.commit("Closed branch %s" % branch, opts.get('user'), opts.get('date'), None, extra={'close':1})
+    hg.clean(repo, "default")
+
+def switch_branch(ui, repo, branch, **opts):
+    """Switch to the named branch"""
+    if branch not in repo.branchtags():
+        ui.write("Branch %s does not exist! (use 'hg branches' to get a list of branches)\n" % branch)
+        return
+    hg.clean(repo, branch)
+
+cmdtable = {
+        "harvest": (harvest, [], "hg harvest BRANCH_NAME"),
+        "close": (close_branch, [], "hg close BRANCH_NAME"),
+        "switch": (switch_branch, [], "hg switch BRANCH_NAME"),
+}
+
